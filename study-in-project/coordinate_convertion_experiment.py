963e1de5971a164dcb9eb1e21577ee1f3b0c5569
BigShrimp <72507432+xiamingtx@users.noreply.github.com>
2023-03-14 11:05:41 +0800

    utm坐标转换到las坐标下

--- study-in-project/coordinate_convertion_experiment.py
+++ study-in-project/coordinate_convertion_experiment.py
@@ -15,7 +15,8 @@ import utm
 import numpy as np
 
 origin = np.array([0, 0, 0])  # 原点
-origin_las_coordinate = np.array([440411.8745000544, 4437551.29303103, 34.8353])  # 点云坐标中拿到的x, y, z
+# 这里做个坐标拟合的示例 将x, y, z比原点的utm坐标都大了10
+origin_las_coordinate = np.array([440421.8745000544, 4437561.29303103, 44.8353])  # 点云坐标中拿到的x, y, z
 start_lat_lon_alt = (40.086133262333, 116.30104133683, 34.8353)  # 设定原点处的latitude、longitude、altitude
 start_utm = np.append(np.array(utm.from_latlon(*start_lat_lon_alt[:2])[:2]), start_lat_lon_alt[-1])
 
@@ -23,10 +24,13 @@ delta = origin_las_coordinate - start_utm  # 起始las坐标和utm的误差
 
 current_lat_lon_alt = (40.086208388333, 116.30099986583, 33.0813)  # 当前位置处的latitude与longitude
 current_utm = np.append(np.array(utm.from_latlon(*current_lat_lon_alt[:2])[:2]), current_lat_lon_alt[-1])
-current_relative_utm = current_utm - start_utm + delta
+current_relative_utm = current_utm - start_utm
+current_las = current_utm + delta
 
-print(start_utm)
-print(current_utm)
-print(current_relative_utm)
+print('起始坐标(原点)的las坐标:', origin_las_coordinate)
+print('起始坐标(原点)的utm坐标:', start_utm)
+print('当前点的utm坐标:', current_utm)
+print('当前点相对原点的utm坐标', current_relative_utm)
 
 print('distance: ', np.sqrt(np.sum(np.square(current_relative_utm - origin))))
+print('在las坐标系下的位置: ', current_las)
