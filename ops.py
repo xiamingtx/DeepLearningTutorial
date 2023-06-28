830ffd21ce443ee77f44ce5d6932b8a91b3ac943
BigShrimp <72507432+xiamingtx@users.noreply.github.com>
2023-06-28 19:22:00 +0800

    GNN-Learning: pyg-tutorial

---
+++ ops.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# file:ops.py
+# author:xm
+# datetime:2023/6/26 16:06
+# software: PyCharm
+
+"""
+this is function  description 
+"""
+
+# import module your need
---
+++ ppp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# file:ppp.py
+# author:xm
+# datetime:2023/6/15 21:24
+# software: PyCharm
+
+"""
+this is function  description 
+"""
+
+# import module your need
